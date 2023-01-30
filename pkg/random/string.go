c50ba2d53fa978d67a34aa36e9b5ce62df7da3d6
Alex Seymour <25187197+LucidUnicorn@users.noreply.github.com>
2023-01-30 13:35:07 +0000

    feat: random string generation

---
+++ pkg/random/string.go
@@ -0,0 +1,37 @@
+package random
+
+import (
+    "crypto/rand"
+    "math/big"
+)
+
+// String generates a cryptographically secure random string.
+type String struct {
+    // CharSet is the set of characters used to generate the string.
+    CharSet []rune
+}
+
+// DefaultStringCharSet is used to generate the string unless overridden in a
+// custom instance of String.
+var DefaultStringCharSet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// NewRandomString creates a new String with default options for quick
+// generation of random strings,
+func NewRandomString() String {
+    return String{
+        CharSet: DefaultStringCharSet,
+    }
+}
+
+// Generate creates the random string according to the configured options.
+func (r *String) Generate(length int) string {
+    str := make([]rune, length)
+    charSetLen64 := int64(len(r.CharSet))
+
+    for i := range str {
+        randInt, _ := rand.Int(rand.Reader, big.NewInt(charSetLen64))
+        str[i] = r.CharSet[randInt.Int64()]
+    }
+
+    return string(str)
+}
---
+++ pkg/random/string_test.go
@@ -0,0 +1,34 @@
+package random
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestString(t *testing.T) {
+    t.Run("Generates unique string values", func(t *testing.T) {
+        r := NewRandomString()
+        var strs []string
+
+        for i := 0; i < 1000; i++ {
+            strs = append(strs, r.Generate(6))
+        }
+
+        var checked []string
+
+        for i := 0; i < len(strs); i++ {
+            assert.NotContains(t, checked, strs[i])
+            checked = append(checked, strs[i])
+        }
+    })
+
+    t.Run("Generates with a custom char set", func(t *testing.T) {
+        r := String{
+            CharSet: []rune("0123456789"),
+        }
+
+        for i := 0; i < 100; i++ {
+            assert.NotSubset(t, []rune(r.Generate(5)), DefaultStringCharSet)
+        }
+    })
+}
