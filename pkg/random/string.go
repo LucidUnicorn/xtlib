d9030f582f272f9492fd7d306b4a0f46117e4fde
Alex Seymour <25187197+LucidUnicorn@users.noreply.github.com>
2023-02-24 11:23:40 +0000

    style: format

--- pkg/random/string.go
+++ pkg/random/string.go
@@ -1,14 +1,14 @@
 package random
 
 import (
-    "crypto/rand"
-    "math/big"
+	"crypto/rand"
+	"math/big"
 )
 
 // String generates a cryptographically secure random string.
 type String struct {
-    // CharSet is the set of characters used to generate the string.
-    CharSet []rune
+	// CharSet is the set of characters used to generate the string.
+	CharSet []rune
 }
 
 // DefaultStringCharSet is used to generate the string unless overridden in a
@@ -16,22 +16,22 @@ type String struct {
 var DefaultStringCharSet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 // NewRandomString creates a new String with default options for quick
-// generation of random strings,
+// generation of random strings.
 func NewRandomString() String {
-    return String{
-        CharSet: DefaultStringCharSet,
-    }
+	return String{
+		CharSet: DefaultStringCharSet,
+	}
 }
 
 // Generate creates the random string according to the configured options.
 func (r *String) Generate(length int) string {
-    str := make([]rune, length)
-    charSetLen64 := int64(len(r.CharSet))
+	str := make([]rune, length)
+	charSetLen64 := int64(len(r.CharSet))
 
-    for i := range str {
-        randInt, _ := rand.Int(rand.Reader, big.NewInt(charSetLen64))
-        str[i] = r.CharSet[randInt.Int64()]
-    }
+	for i := range str {
+		randInt, _ := rand.Int(rand.Reader, big.NewInt(charSetLen64))
+		str[i] = r.CharSet[randInt.Int64()]
+	}
 
-    return string(str)
+	return string(str)
 }
--- pkg/random/string_test.go
+++ pkg/random/string_test.go
@@ -1,34 +1,34 @@
 package random
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestString(t *testing.T) {
-    t.Run("Generates unique string values", func(t *testing.T) {
-        r := NewRandomString()
-        var strs []string
+	t.Run("Generates unique string values", func(t *testing.T) {
+		r := NewRandomString()
+		var strs []string
 
-        for i := 0; i < 1000; i++ {
-            strs = append(strs, r.Generate(6))
-        }
+		for i := 0; i < 1000; i++ {
+			strs = append(strs, r.Generate(6))
+		}
 
-        var checked []string
+		var checked []string
 
-        for i := 0; i < len(strs); i++ {
-            assert.NotContains(t, checked, strs[i])
-            checked = append(checked, strs[i])
-        }
-    })
+		for i := 0; i < len(strs); i++ {
+			assert.NotContains(t, checked, strs[i])
+			checked = append(checked, strs[i])
+		}
+	})
 
-    t.Run("Generates with a custom char set", func(t *testing.T) {
-        r := String{
-            CharSet: []rune("0123456789"),
-        }
+	t.Run("Generates with a custom char set", func(t *testing.T) {
+		r := String{
+			CharSet: []rune("0123456789"),
+		}
 
-        for i := 0; i < 100; i++ {
-            assert.NotSubset(t, []rune(r.Generate(5)), DefaultStringCharSet)
-        }
-    })
+		for i := 0; i < 100; i++ {
+			assert.NotSubset(t, []rune(r.Generate(5)), DefaultStringCharSet)
+		}
+	})
 }
